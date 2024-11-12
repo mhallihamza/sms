c762d2aa1f6890fe0e6576bb964ccf65272088ad
unknown <=>
2024-11-12 19:13:12 +0100

    Modify cors

--- server/apps/api-gateway/src/main.ts
+++ server/apps/api-gateway/src/main.ts
@@ -3,7 +3,11 @@ import { ApiGatewayModule } from './api-gateway.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // Use specific domains in production
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
